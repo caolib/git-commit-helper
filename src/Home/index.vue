cadc6cf13920e5baf75f0e7564926fbbfb16bb8d
孤独的Lonely <1265501579@qq.com>
2025-01-06 01:11:01 +0800

    ✨ feat: 优化界面显示

--- src/Home/index.vue
+++ src/Home/index.vue
@@ -46,7 +46,8 @@ watch(selectedType, (newValue) => {
 });
 
 const onCopy = () => {
-  message.success('复制成功');
+  // message.success('复制成功');
+  utools.hideMainWindow();
   utools.outPlugin();
 };
 
@@ -55,52 +56,91 @@ const onCopy = () => {
 </script>
 
 <template>
-  <div class="commit-generator">
-    <div style="display: flex; align-items: center; margin-bottom: 10px;">
-      <!-- 提交信息类型 -->
-      <a-select v-model:value="selectedType" style="width: 500px;margin-right: 10px;" placeholder="选择 commit 类型"
-        allow-clear show-search>
-        <a-select-option v-for="type in commitTypes" :key="type.value" :value="type.value">
-          {{ type.icon }} {{ type.label }}
-        </a-select-option>
-      </a-select>
+  <div style="display: flex; justify-content: center; margin-bottom: 10px;">
+    <div class="commit-generator">
+      <!-- 第一行 -->
+      <div class="row1" style="display: flex; gap: 10px;width:80vw;">
+        <!-- 提交信息类型 -->
+        <a-select v-model:value="selectedType" style="flex:7;" placeholder="选择类型" allow-clear show-search>
+          <a-select-option v-for="type in commitTypes" :key="type.value" :value="type.value">
+            {{ type.icon }} {{ type.label }}
+          </a-select-option>
+        </a-select>
+        <!-- 输入的范围 -->
+        <a-input v-model:value="scope" style="flex:2;box-shadow: 0px 4px 4px rgb(0 0 0 / 10%);" placeholder="范围（可选）" />
+        <!-- 图标类型 -->
+        <a-select v-model:value="selectedIcon" style="flex:1" placeholder="选择图标">
+          <a-select-option v-for="icon in iconOptions" :key="icon.value" :value="icon.value">
+            {{ icon.label }}
+          </a-select-option>
+        </a-select>
+      </div>
 
-      <!-- 图标类型 -->
-      <a-select v-model:value="selectedIcon" style="width: auto;" placeholder="选择图标">
-        <a-select-option v-for="icon in iconOptions" :key="icon.value" :value="icon.value">
-          {{ icon.label }}
-        </a-select-option>
-      </a-select>
+      <!-- 第二行 -->
+      <div class="row2" style="display: flex;padding: 10px; align-items: center;width: 80vw;">
+        <!-- 提交信息 -->
+        <a-textarea v-model:value="commitMessage" placeholder="输入提交信息概述" :rows="3" />
+      </div>
+
+      <!-- 第三行 -->
+      <div class="row3" style="display: flex;padding: 10px; flex-direction: column;">
+        <!-- 提示文本 -->
+        <a-typography-paragraph class="commit-msg" style="color: gray;font-size: 14px;">
+          下面分别是生成的提交信息和git提交代码，点击即可复制
+        </a-typography-paragraph>
+        <!-- 生成的提交信息 -->
+        <a-typography-paragraph class="commit-msg" :copyable="{ text: generatedCommitMessage, onCopy: onCopy }">
+          {{ generatedCommitMessage }}
+        </a-typography-paragraph>
+        <!-- 生成commit代码拼接信息 -->
+        <a-typography-paragraph class="commit-msg" :copyable="{ text: generatedGitCommitMessage, onCopy: onCopy }">
+          {{ generatedGitCommitMessage }}
+        </a-typography-paragraph>
+      </div>
     </div>
-
-    <div style="display: flex; align-items: center; margin-bottom: 10px;">
-      <!-- 输入的范围 -->
-      <a-input v-model:value="scope" style="width: 570px; margin-bottom: 10px;" placeholder="输入范围（可选）" />
-    </div>
-
-    <div style="display: flex; align-items: center; margin-bottom: 10px;">
-      <!-- 输入的提交信息 -->
-      <a-textarea v-model:value="commitMessage" style="width: 570px; margin-bottom: 10px;" placeholder="输入提交信息概述"
-        :rows="3" />
-    </div>
-
-    <!-- 提交信息文本 -->
-    <div style="margin-bottom: 10px;">
-      <a-typography-paragraph class="commit-msg" :copyable="{ text: generatedCommitMessage, onCopy: onCopy }"
-        style="font-size: 20px; white-space: pre-wrap;">
-        {{ generatedCommitMessage }}
-      </a-typography-paragraph>
-      <a-typography-paragraph class="commit-msg" :copyable="{ text: generatedGitCommitMessage, onCopy: onCopy }"
-        style="font-size: 20px; white-space: pre-wrap;">
-        {{ generatedGitCommitMessage }}
-      </a-typography-paragraph>
-    </div>
-
   </div>
 </template>
 
 <style>
 .commit-generator {
   padding: 10px 28px;
+  margin: 10px;
+}
+
+div.ant-select {
+  margin: 10px;
+}
+
+input.ant-input {
+  height: 32px;
+}
+
+.row1 {
+  align-items: center;
+}
+
+/* 输入的提交信息 */
+textarea.ant-input {
+  height: 30vh;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, .1);
+
+}
+
+/* 可复制文本 */
+.commit-msg {
+  font-size: 20px;
+  width: fit-content;
+  background: white;
+  color: #c678dd;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgb(0 0 0 / 10%);
+  white-space: pre-wrap;
+}
+
+/* 下拉列表 */
+.ant-select-selector {
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, .1);
 }
 </style>
